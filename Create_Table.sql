94295549519b8b081f22f4577cd7469b86882f53
znwafae <92216706+ayoub-codes@users.noreply.github.com>
2022-05-05 12:58:10 +0100

    insert finished

--- Create_Table.sql
+++ Create_Table.sql
@@ -1,2 +1,8 @@
 use testDB;
-create table customers;
+create table customers(
+    id int not null auto_increment,
+    name varchar(255),
+    primary key(id),
+    salary int,
+    address varchar(255)
+);
