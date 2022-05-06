0a614b3ed50881a4781edb2f155f0d10e648a636
znwafae <92216706+ayoub-codes@users.noreply.github.com>
2022-05-06 17:51:56 +0100

    exercice 10 done

--- test/exercice10.sql
+++ test/exercice10.sql
@@ -2,4 +2,4 @@ select nom from produit,puf
 
 inner join produit on produit.id=puf.idproduit
 
-order by ville; 
+ 
--- test/exercice9.sql
+++ test/exercice9.sql
@@ -2,7 +2,7 @@ SELECT nom from produit,puf
 
 inner join produit on produit.id=puf.idproduit
 
-where ville="paris" or ville="creteil" and couleur="rouge";
+where (ville="paris" or ville="creteil") and couleur="rouge";
 
 
 
