e40aed9c6dd78d43e35345edb85fd6020842a59f
znwafae <92216706+ayoub-codes@users.noreply.github.com>
2022-05-06 17:15:42 +0100

    added

--- test/exercice8.sql
+++ test/exercice8.sql
@@ -2,7 +2,6 @@
 
 SELECT id FROM fournisseur,produit,puf,usine
 
-inner join produit on produit.id=puf.idproduit
 inner join fournisseur on fournisseur.id=puf.IdFornisseur
 inner join usine on usine.id=puf.idusine
 
--- test/exercice9.sql
+++ test/exercice9.sql
@@ -1,6 +1,9 @@
 SELECT nom from fornisseur,puf,usine
 
 inner join fournisseur on fournisseur.id=puf.idfornisseur
+inner join usine on usine.id=puf.idusine
+
+where ville="paris" or ville="creteil" and couleur="rouge";
 
 
 
